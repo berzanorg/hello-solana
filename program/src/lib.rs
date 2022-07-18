13609ed2b4f9395c6072de50ff6cecb5cf27bb21
Berzan <berzan@tutanota.com>
2022-07-18 13:09:19 +0300

    add: borsh and Counter struct

--- program/src/lib.rs
+++ program/src/lib.rs
@@ -1,7 +1,9 @@
+use borsh::{BorshSerialize, BorshDeserialize};
 use solana_program::{
-    account_info::AccountInfo,
+    account_info::{AccountInfo, next_account_info},
     entrypoint,
     entrypoint::ProgramResult,
+    program_error::ProgramError,
     msg,
     pubkey::Pubkey,
 };
@@ -13,8 +15,24 @@ fn process_instruction(
     accounts: &[AccountInfo],
     instruction_data: &[u8],
 ) -> ProgramResult {
+    // Iterating accounts is safer than indexing
+    let accounts_iter = &mut accounts.iter();
+
+    // Get the account to say hi
+    let account = next_account_info(accounts_iter)?;
+
+    // The account must be owned by the program in order to modify its data
+    if account.owner != program_id {
+        msg!("Account does not have the correct Program ID");
+        return Err(ProgramError::IncorrectProgramId);
+    }
     
-    msg!("Hello, Solana!");
+    msg!("Hi, Solana!");
 
     Ok(())
+}
+
+#[derive(BorshDeserialize, BorshSerialize, Debug)]
+pub struct Counter {
+    pub count: u32,
 }
